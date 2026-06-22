5af5b58f60dedfb6b8b63ec0f4a659c27dc987cc
Avik Sarkar <avik26122@gmail.com>
2026-06-22 20:45:13 +0530

    some prompt and config changes

--- chat/general_chat.py
+++ chat/general_chat.py
@@ -7,7 +7,7 @@ def general_chat(
 ):
 
     prompt = f"""
-
+give a helpful and elaborated response to the user query giving information about the query in detail.and also use the previous conversation to give a more accurate and helpful response to the user query.also dont report user about no previous data if you dont have any previous data just give a response to the user query without using any previous data if you dont have any previous data.
 Previous conversation:
 
 {memory}
